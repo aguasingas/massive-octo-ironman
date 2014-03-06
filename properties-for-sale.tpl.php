d7bc00f52c6148320991bcb51342e6ae3b77c426
Modesto Caballero <hola@modestocaballero.com>
2014-03-06 02:08:09 +0100

    Added a template for properties_for_sale block.

---
+++ properties-for-sale.tpl.php
@@ -0,0 +1,18 @@
+<div class="numbers">
+  <div class="visits">
+    <div class="value"><?php print $visits; ?></div>
+    <div class="label"><?php print t('Visits'); ?></div>
+  </div>
+  <div class="favorites">
+    <div class="value"><?php print $favorites; ?></div>
+    <div class="label"><?php print t('Favourited'); ?></div>
+  </div>
+  <div class="offers">
+    <div class="value"><?php print $offers; ?></div>
+    <div class="label"><?php print t('Offers made'); ?></div>
+  </div>
+  <div class="showings">
+    <div class="value"><?php print $showings; ?></div>
+    <div class="label"><?php print t('Showings requested'); ?></div>
+  </div>
+</div>
