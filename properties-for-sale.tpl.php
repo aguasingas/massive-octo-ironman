05a5a78aef59a1797de16393e5cef115dafa7de5
Modesto Caballero <hola@modestocaballero.com>
2014-03-14 00:49:18 +0100

    Added a wrapper div around properties_for_sale block.

--- properties-for-sale.tpl.php
+++ properties-for-sale.tpl.php
@@ -16,38 +16,45 @@
  *
  *
  */
-if ($form) {?>
-<div class="form">
-  <?php print $form; ?>
-</div>
-<?php }
-if ($social) {?>
-<div class="social">
-  <?php print $social; ?>
-</div>
-<?php }?>
-<div class="numbers">
-  <div class="visits">
-    <div class="value"><?php print $visits; ?></div>
-    <div class="label"><?php print t('Visits'); ?></div>
+?>
+<div id="properties-for-sale-statistics">
+  <?php
+  if ($form) {
+    ?>
+    <div class="form">
+      <?php print $form; ?>
+    </div>
+  <?php
+  }
+  if ($social) {
+    ?>
+    <div class="social">
+      <?php print $social; ?>
+    </div>
+  <?php } ?>
+  <div class="numbers">
+    <div class="visits">
+      <div class="value"><?php print $visits; ?></div>
+      <div class="label"><?php print t('Visits'); ?></div>
+    </div>
+    <div class="downloads">
+      <div class="value"><?php print $downloads; ?></div>
+      <div class="label"><?php print t('Downloads'); ?></div>
+    </div>
+    <div class="favorites">
+      <div class="value"><?php print $favorites; ?></div>
+      <div class="label"><?php print t('Favourited'); ?></div>
+    </div>
+    <div class="offers">
+      <div class="value"><?php print $offers; ?></div>
+      <div class="label"><?php print t('Offers made'); ?></div>
+    </div>
+    <div class="showings">
+      <div class="value"><?php print $showings; ?></div>
+      <div class="label"><?php print t('Showings requested'); ?></div>
+    </div>
   </div>
-  <div class="downloads">
-    <div class="value"><?php print $downloads; ?></div>
-    <div class="label"><?php print t('Downloads'); ?></div>
+  <div class="chart">
+    <?php print $chart; ?>
   </div>
-  <div class="favorites">
-    <div class="value"><?php print $favorites; ?></div>
-    <div class="label"><?php print t('Favourited'); ?></div>
-  </div>
-  <div class="offers">
-    <div class="value"><?php print $offers; ?></div>
-    <div class="label"><?php print t('Offers made'); ?></div>
-  </div>
-  <div class="showings">
-    <div class="value"><?php print $showings; ?></div>
-    <div class="label"><?php print t('Showings requested'); ?></div>
-  </div>
-</div>
-<div class="chart">
-  <?php print $chart; ?>
 </div>
